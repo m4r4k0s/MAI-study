1440b0cab8ccbdbdb45cffb17efacf7760467f7e
m4r4k0s <52090995+m4r4k0s@users.noreply.github.com>
2022-07-19 17:27:27 +0300

    Add files via upload

---
+++ sem3/ISRPPS/lab9/Laba5/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Laba5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 233);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Model: Journal";
+            
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "View: WinForm";
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 75);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Controller: class JournalForMVC";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 253);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 23);
+            this.textBox1.TabIndex = 8;
+            
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 299);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(358, 55);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "";
+            
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 31);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(46, 105);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(358, 121);
+            this.panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 380);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ sem3/ISRPPS/lab9/Laba5/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = CardforMVS.get_title();
+            textBox2.Text = CardforMVS.get_info();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = CardforMVS.get_info();
+            textBox1.Text = CardforMVS.get_title();
+            CardforMVS.reset();
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CardforMVS.ch_title(textBox1.Text);
+            CardforMVS.ch_info(textBox2.Text);
+            CardforMVS.Save();
+        }        
+    }
+
+    class Journal
+    {
+        private static Journal journal = new Journal();
+
+        private string _title;
+        private string Text;
+
+        static public string title
+        {
+            get { return journal._title; }
+            set { journal._title = value; }
+        }
+        static public string info
+        {
+            get { return journal.Text; }
+            set { journal.Text = value; }
+        }
+
+        private Journal()
+        {
+            _title = "Cool article";
+            Text = "some info";
+        }
+    }
+
+
+    static class CardforMVS
+    {
+        static string title;
+        static string info;
+
+        static public void ch_title(string s)
+        {
+            title = s;
+        }
+
+        static public void ch_info(string s)
+        {
+            info = s;
+        }
+
+        static public void Save()
+        {
+            Journal.title = title;
+            Journal.info = info;
+        }
+
+        static public string get_title() { return Journal.title; }
+        static public string get_info() { return Journal.info; }
+
+        static public void reset()
+        {
+            title = String.Empty;
+            info = String.Empty;
+        }
+    }
+}
