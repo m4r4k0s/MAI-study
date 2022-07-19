54673c018e30f75080fe81420874f22c9bb755e8
m4r4k0s <52090995+m4r4k0s@users.noreply.github.com>
2022-07-19 17:35:31 +0300

    Add files via upload

---
+++ sem3/ISRPPS/lab9/Laba14/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Laba14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(418, 8);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(117, 363);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Рисунок";
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(9, 173);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(23, 23);
+            this.panel2.TabIndex = 6;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(37, 103);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(32, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "F";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(8, 76);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(66, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Эллипс";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 50);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ломаная";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 23);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Линия";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(14, 14);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(397, 358);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 391);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ sem3/ISRPPS/lab9/Laba14/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba14
+{
+    public partial class Form1 : Form
+    {
+        ColorDialog clrdlg = new ColorDialog();
+        Pen p;
+
+        Point[,] line = new Point[100, 2];
+        int line_rec = 0;
+
+        Point[,] poly = new Point[100, 100];
+        int[] poly_p = new int[100];
+        int poly_rec = 0;
+
+        int[,] ell = new int[100, 4];
+        bool[] ell_f = new bool[100];
+        int ell_rec = 0;
+
+        Point[,] bez = new Point[100, 4];
+        int bez_rec = 0;
+        int bez_p = 0;
+
+
+        int type = 0;
+        bool is_draw = false;
+
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+            clrdlg.Color = Color.Red;
+            p = new Pen(clrdlg.Color, 2.0f);
+            panel2.BackColor = clrdlg.Color;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            draw();
+        }
+
+        void draw()
+        {
+            groupBox1.Size = new Size(groupBox1.Size.Width, this.Size.Height - 55);
+            panel1.Size = new Size(this.Size.Width - 140, this.Size.Height - 60);
+            g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            for (int i = 0; i < line_rec; i++)
+                g.DrawLine(p, line[i, 0], line[i, 1]);
+
+            for (int i = 0; i < poly_rec; i++)
+            {
+                for (int j = 0; j + 1 < poly_p[i]; j++)
+                    g.DrawLine(p, poly[i, j], poly[i, j + 1]);
+            }
+
+            for (int i = 0; i < ell_rec; i++)
+            {
+                if (ell_f[i] == true)
+                    g.FillEllipse(new SolidBrush(clrdlg.Color), ell[i, 0] - ell[i, 2], ell[i, 1] - ell[i, 3], 2 * ell[i, 2], 2 * ell[i, 3]);
+                else g.DrawEllipse(p, ell[i, 0] - ell[i, 2], ell[i, 1] - ell[i, 3], 2 * ell[i, 2], 2 * ell[i, 3]);
+            }
+
+            for (int i = 0; i < bez_rec; i++)
+            {
+                g.DrawBezier(p, bez[i, 0], bez[i, 1], bez[i, 2], bez[i, 3]);
+            }
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (is_draw == false)
+            {
+                if (type == 1 && poly_rec < 100)
+                {
+                    is_draw = true;
+                    poly_rec++;
+                    poly_p[poly_rec - 1] = 2;
+                    poly[poly_rec - 1, 0] = new Point(e.X, e.Y);
+                    poly[poly_rec - 1, poly_p[poly_rec - 1] - 1] = new Point(e.X, e.Y);
+                    draw();
+                }
+                if (type == 2 && ell_rec < 100)
+                {
+                    is_draw = true;
+                    ell_rec++;
+                    ell_f[ell_rec - 1] = checkBox1.Checked;
+                    ell[ell_rec - 1, 0] = e.X;
+                    ell[ell_rec - 1, 1] = e.Y;
+                    ell[ell_rec - 1, 2] = 10;
+                    ell[ell_rec - 1, 3] = 10;
+                    draw();
+                }
+                if (type == 3 && bez_rec < 100)
+                {
+                    is_draw = true;
+                    bez_rec++;
+                    bez[bez_rec - 1, 0] = new Point(e.X, e.Y);
+                    bez[bez_rec - 1, 1] = new Point(e.X + 10, e.Y + 25);
+                    bez[bez_rec - 1, 2] = new Point(e.X + 25, e.Y + 10);
+                    bez[bez_rec - 1, 3] = new Point(e.X, e.Y);
+                    bez_p = 1;
+                    draw();
+                }
+            }
+            else if (is_draw == true)
+            {
+                if (type == 1)
+                {
+                    if (e.Button == MouseButtons.Right)
+                        is_draw = false;
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        if (poly_p[poly_rec - 1] < 100)
+                        {
+                            poly[poly_rec - 1, poly_p[poly_rec - 1] - 1] = new Point(e.X, e.Y);
+                            poly_p[poly_rec - 1]++;
+                            poly[poly_rec - 1, poly_p[poly_rec - 1] - 1] = new Point(e.X, e.Y);
+                        }
+                        else is_draw = false;
+                    }
+
+                }
+                if (type == 2)
+                {
+                    is_draw = false;
+                }
+                if (type == 3 && bez_rec < 100)
+                {
+                    bez[bez_rec - 1, bez_p] = new Point(e.X, e.Y);
+                    bez_p++;
+                    if (bez_p == 4) { is_draw = false; bez_p = 0; }
+                    draw();
+                }
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && is_draw == false)
+            {
+                if (type == 0 && line_rec < 100)
+                {
+                    is_draw = true;
+                    line_rec++;
+                    line[line_rec - 1, 0] = new Point(e.X, e.Y);//(e.X-110, e.Y-10);
+                    line[line_rec - 1, 1] = new Point(e.X, e.Y);
+                    draw();
+                }
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (is_draw)
+            {
+                if (type == 0)
+                {
+                    line[line_rec - 1, 1] = new Point(e.X, e.Y);
+                    draw();
+                }
+                if (type == 1)
+                {
+                    poly[poly_rec - 1, poly_p[poly_rec - 1] - 1] = new Point(e.X, e.Y);
+                    draw();
+                }
+                if (type == 2)
+                {
+                    ell[ell_rec - 1, 2] = Math.Abs(ell[ell_rec - 1, 0] - e.X);
+                    ell[ell_rec - 1, 3] = Math.Abs(ell[ell_rec - 1, 1] - e.Y);
+                    draw();
+                }
+                if (type == 3)
+                {
+                    bez[bez_rec - 1, bez_p] = new Point(e.X, e.Y);
+                    draw();
+                }
+            }
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (is_draw)
+            {
+                if (type == 0)
+                {
+                    is_draw = false;
+                }
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Enabled == true) type = 1;
+            is_draw = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            line_rec = 0;
+            poly_rec = 0;
+            ell_rec = 0;
+            bez_rec = 0;
+            is_draw = false;
+            draw();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Enabled == true) type = 2;
+            is_draw = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Enabled == true) type = 0;
+            is_draw = false;
+        }
+
+        private void panel2_MouseClick(object sender, MouseEventArgs e)
+        {
+            clrdlg.ShowDialog();
+            p = new Pen(clrdlg.Color, 2.0f);
+            panel2.BackColor = clrdlg.Color;
+            draw();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+
+
+}
